9be8fa0148f8e71018dd04f30f7ddbc3ea242661
Franziska <Franziska@PC1996.eduroam.hs-furtwangen.de>
2018-03-28 16:21:02 +0200

    Aufgabe0 gelöscht

--- Aufgabe0/Aufgabe0.js
+++
@@ -1,6 +0,0 @@
-function Textbox() {
-    var name = prompt("Bitte gib deinen Namen ein?", "");
-    if (name != null) {
-        document.getElementById("Name").innerHTML = "Hi " + name + ". Hier gibts noch nichts spannendes zu sehen.";
-    }
-}
--- Aufgabe0/Aufgabe0.ts
+++
@@ -1,9 +0,0 @@
-
-        
-function Textbox(): void {
-  var name: string = prompt("Bitte gib deinen Namen ein?", "");
-  if (name != null) {
-     document.getElementById("Name").innerHTML = "Hi " + name + ". Hier gibts noch nichts spannendes zu sehen.";
-     }
-  }
-    
